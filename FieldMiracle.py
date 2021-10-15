a7f9865621d3b4a08442faf72714ddf7cf876e60
LizzieGri <liza-gri2001@yandex.ru>
2021-10-15 14:25:14 +0300

    Write question in file

---
+++ FieldMiracle.py
@@ -0,0 +1,6 @@
+def write_question(question, answer):
+    with open('question.txt', 'a', encoding = 'utf-8') as f:
+            f.write(question + ';' + answer + '\n')
+    return question + ';' + answer
+
+
