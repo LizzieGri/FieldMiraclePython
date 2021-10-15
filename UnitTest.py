4da0fe9e73901c7d6725f61c9dfac186d765240a
LizzieGri <liza-gri2001@yandex.ru>
2021-10-15 15:05:11 +0300

    Невозможно ввести несколько ответов сразу и выйти из режима ввода

--- UnitTest.py
+++ UnitTest.py
@@ -3,8 +3,11 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(FieldMiracle.write_question("Самая длинная река в мире", "амазонка"), "Самая длинная река в мире;амазонка")
+    # def test_something(self):
+    #     self.assertEqual(FieldMiracle.write_question("Самая длинная река в мире", "амазонка"), "Самая длинная река в мире;амазонка")
+
+    def test_many_quit(self):
+        self.assertEqual(FieldMiracle("quit", ""), "")
 
 
 if __name__ == '__main__':
