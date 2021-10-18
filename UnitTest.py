e2a23a8762b6a6e8d17db294f33e56d1200c83d5
LizzieGri <liza-gri2001@yandex.ru>
2021-10-18 14:10:57 +0300

    При вводе верного слова программа не завершается должным образом

--- UnitTest.py
+++ UnitTest.py
@@ -15,8 +15,12 @@ class MyTestCase(unittest.TestCase):
     # def test_encryption(self):
     #     self.assertEqual(FieldMiracle.output_question(),'********')
 
-    def test_answ(self):
-        self.assertEqual(FieldMiracle.answ('а'), 'а*а****а')
+    # def test_answ(self):
+    #     self.assertEqual(FieldMiracle.answ('а'), 'а*а****а')
+
+    def test_win(self):
+        self.assertEqual(FieldMiracle.answ('амазонка'), 'Вы правильно угадали слово!')
+
 
 
 if __name__ == '__main__':
